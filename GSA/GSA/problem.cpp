8848977f8956b9b7552ff94a552b89799e68af3a
cedol59 <cedol59@gmail.com>
2016-02-12 11:26:39 +0100

    ajout ID probleme

--- GSA/GSA/problem.cpp
+++ GSA/GSA/problem.cpp
@@ -6,26 +6,68 @@ Problem::Problem()
 Problem::~Problem()
 {}
 
+string Problem::method()
+{
+    switch (this.id)
+    {
+    case 1:
+        {
+            this.LowerLimit=-5.12;
+            this.UpperLimit=5.12;
+            return "Rastrigin";
+        }
+    case 2:
+        {
+            this.LowerLimit=-5.12;
+            this.UpperLimit=5.12;
+            return "Roulette";
+        }
+    case 3:
+        {
+            this.LowerLimit=-5.12;
+            this.UpperLimit=5.12;
+            return "Rang";
+        }
+    case 4:
+        {
+            this.LowerLimit=-5.12;
+            this.UpperLimit=5.12;
+            return "Tournoi";
+        }
+/*    case 5:
+        {
+
+        }
+    case 6:
+        {
+
+        }*/
+    }
+}
+
 ostream& operator<< (ostream& os, const Problem& pb)
 {
  return
  	 os << "lower limit : " << pb.LowerLimit << endl
         << "upper limit : " << pb.UpperLimit << endl
         << "dimension : " << pb._dimension << endl
-        << "ID :";
+        << "method : " << pb.method() ;
 }
 
 istream& operator>> (istream& is, Problem& pb)
 {
- return
- 	 is >> pb.LowerLimit
-        >> pb.UpperLimit
-        >> pb._dimension;
+    is >> pb._dimension
+        >> pb.id;
+    pb.method();
+    return is;
 }
 
 Problem& Problem::operator=  (const Problem& pbm)
 {
-
+    this.LowerLimit=pbm.LowerLimit;
+    this.UpperLimit=pbm.UpperLimit;
+    this._dimension=pbm._dimension;
+    this.id=pbm.id;
 }
 
 bool Problem::operator== (const Problem& pbm) const
